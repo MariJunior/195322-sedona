4cfbe2976896aefc512d41a7c96fe3fbe04e903e
Marina Kalyuzhnaya <marinakaluzhnaya13@gmail.com>
2018-03-29 00:16:20 +0300

    Проверка формы на валидность, запись данных в localStorage

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,63 @@
 var searchButton = document.querySelector(".reserv__btn");
-var popupForm = document.querySelector(".reserv-modal");
+var popupSection = document.querySelector(".reserv-modal");
 
-if (searchButton && popupForm) {
-  searchButton.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    popupForm.classList.toggle("reserv-modal--show");
-    popupForm.classList.toggle("reserv-modal--hide");
-  });
+var form = popupSection.querySelector(".reserv-modal__form");
+var formFields = popupSection.querySelectorAll(".reserv-modal__input");
+// var arriceDate = popupSection.querySelector("[name=arrival_date]");
+// var departureDate = popupSection.querySelector("[name=departure_date]");
+var adults = popupSection.querySelector("[name=adults]");
+var childrens = popupSection.querySelector("[name=children]");
+
+var isStorageSupport = true; 
+var storageAdult = "";
+var storageChildren = "";
+       
+try {
+  storageAdult = localStorage.getItem("adults");
+  storageChildren = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
 }
 
+searchButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popupSection.classList.toggle("reserv-modal--show");
+  popupSection.classList.toggle("reserv-modal--hide");
+});
+
+if(popupSection.classList.contains("reserv-modal--show")) {
+  if (storageAdult && storageChildren) {
+    adults.value = storageAdult;
+    childrens.value = storageChildren;
+  }
+  popupSection.querySelector("[name=arrival_date]").focus();
+} else {
+  popupSection.classList.remove("reserv-modal--error");
+};
+
+form.addEventListener("submit", function(evt) {
+  for(var i = 0, len = formFields.length; i < len; i++) {
+    if(!formFields[i].value) {
+      evt.preventDefault();
+      popupSection.classList.remove("reserv-modal--error");
+      popupSection.offsetWidth = popupSection.offsetWidth;  
+      popupSection.classList.add("reserv-modal--error");
+      formFields[i].focus();
+    } else {
+      if (isStorageSupport) {
+      localStorage.setItem("adults", adults.value);
+      localStorage.setItem("children", childrens.value);
+      }
+    }
+  };
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupSection.classList.contains("reserv-modal--show")) {
+      popupSection.classList.remove("reserv-modal--show");
+      popupSection.classList.remove("reserv-modal--error");
+    }
+  }
+});
